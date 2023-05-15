ea71c5c1f062661684fc3dd9c411c69eeff21be4
eeminov1995 <e.eminov1995@mail.ru>
2023-05-15 21:31:09 +0300

    Add new files
    - feat: Добавил новые файлы

---
+++ 4.2.Вложенные_списки_Часть_1/4.2.15.py
@@ -0,0 +1,4 @@
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+[sub_list.reverse() for sub_list in list1]
+
+print(list1)
---
+++ 4.3.Вложенные_списки_Часть_2/4.3.8.py
@@ -0,0 +1,6 @@
+# put your python code here
+n = int(input())
+
+for _ in range(n):
+    lst = [i for i in range(1, n + 1)]
+    print(lst)
